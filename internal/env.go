f2c350d2460a4fcf5c9605c4cce80ec79fe95cb5
orenoi <170115589+orenoi@users.noreply.github.com>
2025-08-02 07:15:15 +0400

    add PULSARSHIP_DEBUG env var for flags

--- internal/env.go
+++ internal/env.go
@@ -6,4 +6,5 @@ var (
 	HOME_ENV          = os.Getenv("HOME")
 	PULSARSHIP_CONFIG = os.Getenv("PULSARSHIP_CONFIG")
 	PULSARSHIP_SHELL  = os.Getenv("PULSARSHIP_SHELL")
+	PULSARSHIP_DEBUG  = os.Getenv("PULSARSHIP_DEBUG")
 )
